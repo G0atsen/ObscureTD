6378e6925d589709e44fa891b40133381b6042e3
Alice Seremane <seremane.t@gmail.com>
2018-08-13 11:24:13 +0200

    Grid
    These are the scripts to generate the grid and place tiles

---
+++ Obscure TD/Assets/_scripts/itemPlacer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemPlacer : MonoBehaviour {
+
+    private grid grid;
+     public Camera cam;
+    public GameObject tilePrefab;
+    public int tilesCreated;
+
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("stareted");
+            RaycastHit hitInfo;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            print("click");
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+            print(hitInfo.point);
+            tilesCreated += 1;
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        var clone = Instantiate(tilePrefab, finalPosition,Quaternion.identity);
+        clone.name = "tile" + tilesCreated;
+       // GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	
+}
